6488754b91c2680d374876f4b82b8697c3dcb685
Ray63s <raynnaveed13@gmail.com>
2025-11-28 12:13:09 +0500

    adding calculator from 27nov

---
+++ main.py
@@ -0,0 +1,35 @@
+# making a calculator!
+
+
+def sub_add():
+
+    a = float(input("enter first number!"))
+    b = float(input("enter second number!"))
+
+    print(a + b)
+
+def mul():
+
+    a = float(input("first number!"))
+    b = float(input("second number"))
+
+    print(a*b)
+
+def div():
+
+    a = float(input("enter dividend!"))
+    b = float(input("enter divisor!"))
+
+    print(a / b)
+
+x = float(input("do you want to sub_add or mul or div ?!\n for sub_add press 1! \n for mul press 2! \n for div press 3!"))
+
+if x == 1:
+    sub_add()
+elif x == 2:
+    mul()
+elif x == 3:
+    div()
+else:
+    print("error, Try Again!")
+
